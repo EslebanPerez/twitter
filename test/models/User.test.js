6cca28e82c60caa953b55ebade20c551269c1602
EslebanPerez <eslebanperez@gmail.com>
2022-04-25 18:27:38 -0500

    Agregando prueba de unidad de lo que se espera obtener como resultado

---
+++ test/models/User.test.js
@@ -0,0 +1,15 @@
+describe("Unit Test for User class",() =>{
+    test('Create an User object', () =>{
+        // Aquí invocas el código que vas a usar en tu app
+        const user = new User(1, "EslebanPerez", "Esleban", "Bio", "dateCreated", "lastUpdated")
+        
+        // Aquí validad los resultados de ese código
+        // Esta es una comparación que va a igualar el valor de la izquierda conoel valor de la derecha (valor esperado)
+        expect(user.id).toBe(1)
+        expect(user.username).toBe("EslebanPerez")
+        expect(user.name).toBe("Esleban")
+        expect(user.bio).toBe("Bio")
+        expect(user.dateCreated).toBe("dateCreated")
+        expect(user.lastUpdated).toBe("lastUpdated")
+    })
+})

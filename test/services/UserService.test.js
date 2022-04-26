3485b301daa15bb619e2239e7cf0697298d26ace
EslebanPerez <eslebanperez@gmail.com>
2022-04-26 04:54:53 -0500

    Requerimiento 2 - práctica 4: Agregando método estáico y regresando una lista

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -1,3 +1,4 @@
+const UserService = require ('../../app/services/UserService')
 
 describe ("Test for UserService", () =>{
 
@@ -7,5 +8,15 @@ describe ("Test for UserService", () =>{
         expect(user.name).toBe("Esleban")
         expect(user.id).toBe(1)
         expect(user.bio).not.toBeUndefined()
+    });
+    test("2. Get all users data in a list", () =>{
+        const user = UserService.create(1, "EslebanPerez", "Esleban")
+        const userInfoInlist = UserService.getInfo(user)
+        expect(userInfoInlist[0]).toBe(1)
+        expect(userInfoInlist[1]).toBe("EslebanPerez")
+        expect(userInfoInlist[2]).toBe("Esleban")
+        expect(userInfoInlist[3]).toBe("Sin bio")
+
     })
+    
 })
